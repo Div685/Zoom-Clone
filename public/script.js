b0f78603ebc27a7e9de64569574fb89e96d9cccc
Div685 <divyeshpatel685@gmail.com>
2020-08-21 00:22:52 +0300

    style app with css

--- public/script.js
+++ public/script.js
@@ -1,12 +1,12 @@
 const socket = io("/");
 const myVideo = document.createElement("video");
 const videoGrid = document.getElementById("video-grid");
-myVideo.muted = true
+myVideo.muted = true;
 
 var peer = new Peer(undefined, {
-    path="/peerjs",
-    host: "/",
-    port: "3030",
+  path: "/peerjs",
+  host: "/",
+  port: "3030",
 });
 
 navigator.mediaDevices
@@ -17,22 +17,22 @@ navigator.mediaDevices
   .then((stream) => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
-  });
 
-peer.on("call", (call) => {
-  call.answer(stream);
-  const video = document.createElement("video");
-  call.on("stream", (userVideoStream) => {
-    addVideoStream(video, userVideoStream);
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
   });
-});
 
 peer.on("open", (id) => {
   socket.emit("join-room", ROOM_ID, id);
-});
-
-socket.on("user-connected", (userId) => {
-  connectToNewUser(userId, stream);
 });
 
 const connectToNewUser = (userId, stream) => {
