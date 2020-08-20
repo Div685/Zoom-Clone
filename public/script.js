77731177d273f446cbdc11f96e611223bf6b6ee3
Div685 <divyeshpatel685@gmail.com>
2020-08-21 02:10:54 +0300

    final commit

--- public/script.js
+++ public/script.js
@@ -1,14 +1,16 @@
 const socket = io("/");
-const myVideo = document.createElement("video");
 const videoGrid = document.getElementById("video-grid");
+const myVideo = document.createElement("video");
 myVideo.muted = true;
 
 var peer = new Peer(undefined, {
   path: "/peerjs",
   host: "/",
-  port: "3030",
+  port: "3030", //change this to 443 if you are deploying
 });
 
+let myVideoStream;
+const peers = {};
 navigator.mediaDevices
   .getUserMedia({
     video: true,
@@ -27,7 +29,7 @@ navigator.mediaDevices
     });
 
     socket.on("user-connected", (userId) => {
-      connectToNewUser(userId, stream);
+      connecToNewUser(userId, stream);
     });
   });
 
@@ -35,7 +37,9 @@ peer.on("open", (id) => {
   socket.emit("join-room", ROOM_ID, id);
 });
 
-const connectToNewUser = (userId, stream) => {
+// socket.emit("join-room", ROOM_ID);
+
+const connecToNewUser = (userId, stream) => {
   const call = peer.call(userId, stream);
   const video = document.createElement("video");
   call.on("stream", (userVideoStream) => {
@@ -49,4 +53,81 @@ const addVideoStream = (video, stream) => {
     video.play();
   });
   videoGrid.append(video);
+};
+
+let text = $("input");
+console.log(text);
+
+$("html").keydown((e) => {
+  if (e.which == 13 && text.val().length !== 0) {
+    console.log(text.val());
+    socket.emit("message", text.val());
+    text.val("");
+  }
+});
+
+socket.on("createMessage", (message) => {
+  $("ul").append(`<li class="message"><b>user</b/><br/>${message}</li>`);
+  scrollToBottom();
+});
+
+const scrollToBottom = () => {
+  let d = $(".main__chat_window");
+  d.scrollTop(d.prop("scrollHeight"));
+};
+
+//Mute our video
+
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+};
+
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const playStop = () => {
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo();
+  } else {
+    setStopVideo();
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+};
+
+const setStopVideo = () => {
+  const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `;
+  document.querySelector(".main__video_button").innerHTML = html;
+};
+
+const setPlayVideo = () => {
+  const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `;
+  document.querySelector(".main__video_button").innerHTML = html;
 };
--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
-
 const io = require("socket.io")(server);
 const { v4: uuidv4 } = require("uuid");
 const { ExpressPeerServer } = require("peer");
-
 const peerServer = ExpressPeerServer(server, {
   debug: true,
 });
@@ -15,7 +13,7 @@ app.use(express.static("public"));
 
 app.use("/peerjs", peerServer);
 app.get("/", (req, res) => {
-  res.redirect(`/${uuidv4()}`);
+  res.redirect(`/${uuidv4()} `);
 });
 
 app.get("/:room", (req, res) => {
@@ -26,7 +24,10 @@ io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId) => {
     socket.join(roomId);
     socket.to(roomId).broadcast.emit("user-connected", userId);
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message);
+    });
   });
 });
 
-server.listen(3030);
+server.listen(process.env.PORT || 3030);
